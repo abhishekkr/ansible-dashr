c38fc3437b3988527111aa4cc97e7ca82771a0ac
AbhishekKr <abhikumar163@gmail.com>
2015-01-05 22:39:21 +0530

    [role] added js-yaml implementation to it

--- www-data/js/main.roles.parser.js
+++ www-data/js/main.roles.parser.js
@@ -2,59 +2,17 @@
 * javascript library capable of parsing Ansible Roles to a desired dictionary
 */
 
-/* regex for Role tokens */
-var RoleRegex = {
-    initiate: /^---\s*$/,
-    add_step: /^-\s*([a-zA-Z0-9]*)\s*:(.*)/,
-    comment: /^\s*#(.*)/,
-    not_blank: /[^\s]+/,
-    task_name: /name:\s*(.*)$/
-};
-
-/* parse line from Role */
-function parseRoleLine(Role, line){
-  if(RoleRegex.add_step.test(line)){
-    Role.task_idx += 1;
-    var match = line.match(RoleRegex.add_step);
-    Role.tasks[Role.task_idx] = {"name": match[1], "task_def": [match[2]]};
-  }
-
-  if(RoleRegex.comment.test(line)){
-    var match = line.match(RoleRegex.comment);
-    Role.comments[Role.task_idx] += match[1];
-
-  }else if(RoleRegex.task_name.test(line)){
-    var match = line.match(RoleRegex.task_name);
-    Role.tasks[Role.task_idx]["name"] = match[1];
-
-  }else if(RoleRegex.initiate.test(line)){
-    console.log("Role definition started.");
-
-  }else if(RoleRegex.not_blank.test(line)){
-    var task_def = Role.tasks[Role.task_idx]["task_def"];
-    task_def.push(line);
-    Role.tasks[Role.task_idx]["task_def"] = task_def;
-  };
-}
-
-/* parse Ansible Role configuration files */
-function parseRole(data){
-  var Role = {tasks: [], comments: [], task_idx: -1};
-  var lines = data.split(/\r\n|\r|\n/);
-
-  for(var line_idx in lines){
-    parseRoleLine(Role, lines[line_idx]);
-  }
-  return Role;
+function rolePath(role_name){
+  return roles_www_path + "/" + role_name + "/tasks/main.yml";
 }
 
 /* parse all roles from a list at given path */
-function parseRoles(roles, path){
+function parseRoles(roles){
   var rolesInfo = {};
   for(var role_idx in roles){
-    var role = roles[role_idx];
-    var roleData = loadURI(path + "/" + role + "/tasks/main.yml");
-    rolesInfo[role] = parseRole(roleData);
+    var role_name = roles[role_idx];
+    var role_uri = rolePath(role_name);
+    rolesInfo[role_name] = YAMLURI2JSON(role_uri);
   }
   return rolesInfo;
 }
@@ -62,20 +20,22 @@ function parseRoles(roles, path){
 /* publishes Roles Details to a given div */
 function publishRoleDetails(rolename, div_id){
   $DOM("#roleName").innerHTML = rolename;
-  var tasks = rolesInfo[rolename].tasks;
+  var tasks = rolesInfo[rolename];
   var innerHTML = "";
   for(var task_idx in tasks){
+    innerHTML += "<tr>"
     var task = tasks[task_idx];
     var task_idx_human = parseInt(task_idx) + 1;
-    innerHTML += "<tr class='task'> <td>#" + task_idx_human + " <b class='task_name'>" + task["name"] + "</b></td>";
-    innerHTML += "<td><ul class='task_defs'>";
-    for(var task_def_idx in task["task_def"]){
-      var task_def = task["task_def"][task_def_idx];
-      var task_def_class = " class='" + task_def + " " + task_idx + " "  + task_idx + "." + task_def_idx + "' ";
-      var task_def_style = " style='' ";
-      innerHTML += "<li" + task_def_class + task_def_style + ">" + task_def + "</li>";
+    var count = 0;
+    for(var inner_task_key in task){
+      if(count == 0){
+        innerHTML += "<td>#" + task_idx + " <i>" + inner_task_key + "</i>: <b>" + task[inner_task_key]  + "<b></td><td>"
+      } else {
+        innerHTML += "<div><i>" + inner_task_key + "</i>: <b>" + task[inner_task_key]  + "</b></div>"
+      }
+      count += 1;
     }
-    innerHTML += "</ul></td></tr>";
+    innerHTML += "</td></tr>"
   }
   $DOM(div_id).innerHTML = innerHTML;
 }
@@ -94,6 +54,6 @@ $(function() {
 
 
 /* parse and update roles */
-var rolesInfo = parseRoles(roles, roles_www_path);
+var rolesInfo = parseRoles(roles);
 publishRoleDetails(roles[0], '#roleDetails')
 
