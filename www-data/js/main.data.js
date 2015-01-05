d666797353ce336262c067342cbe7c3aa11eb9b2
AbhishekKr <abhikumar163@gmail.com>
2015-01-05 17:22:19 +0530

    [main.data] updated relative paths for roles and playbooks

--- www-data/js/main.data.js
+++ www-data/js/main.data.js
@@ -6,7 +6,7 @@
 
 //var playbook = ['pb01', 'pb02', 'pb03', 'pb04', 'pb05'];
 var playbooks = ['pb04', 'pb05'];
-var playbooks_www_path = "/www-data/dummy-ansible-files";
+var playbooks_www_path = "/dummy-ansible-files";
 
 var roles = ['memcached', 'ntp', 'bind', 'mysql', 'nginx', 'redis'];
-var roles_www_path = "/www-data/dummy-ansible-files/roles";
+var roles_www_path = "/dummy-ansible-files/roles";
